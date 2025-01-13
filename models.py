bef28d57ff3705b364783ee58176fce53577a483
Caroline-1707 <k.kunasheva@gmail.com>
2025-01-13 14:10:15 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Text
+from database import Base
+
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    views = Column(Integer, default=0)
+    cooking_time = Column(Integer)
+    ingredients = Column(Text)
+    instructions = Column(Text)
